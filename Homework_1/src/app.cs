8df73a4d515c885c7c7bc0d0c9cb6f2511f2db7d
Krimaxev <m.krilichevsky@yandex.ru>
2025-09-28 15:52:35 +0300

    Add experimental file

---
+++ Homework_1/src/app.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        
+        Console.WriteLine("Перед Вами набор товаров:");
+        Console.Write("Яблоко Груша Абрикос");
+        
+    }
+}
